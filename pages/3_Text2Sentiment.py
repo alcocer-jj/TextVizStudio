b28a2ce008cbc59e17ba70b8d6571087256e0df5
José J. Alcocer <84480542+alcocer-jj@users.noreply.github.com>
2024-11-09 12:13:02 -0800

    Update 3_Text2Sentiment.py

--- pages/3_Text2Sentiment.py
+++ pages/3_Text2Sentiment.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from transformers import pipeline, AutoModelForSequenceClassification, XLMRobertaTokenizer, AutoConfig
 import hashlib
